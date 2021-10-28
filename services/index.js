c326ceb8fe5cd49fde680a8d4d5ae67e997874b9
Ahmed Kholaif <Ahmed_Kholaif@yahoo.com>
2021-10-28 12:47:22 +0200

    Aggregate responses

--- services/index.js
+++ services/index.js
@@ -2,7 +2,19 @@ const axios = require('axios');
 
 
 const getPeople =async()=>{
-  return axios.get('http://swapi.dev/api/people');
+  let responses = [];
+  let url  = 'http://swapi.dev/api/people'
+  do {
+    const peopleResponse = await axios.get(url);
+
+    responses = [...responses,peopleResponse];
+    url = peopleResponse.data.next;
+  } while (url);
+  const result = responses.reduce((agg,res)=>{
+    agg.data.results = [...agg.data.results,...res.data.results]
+    return agg;
+  },{data:{results:[]}})
+  return result;
 }
 
 module.exports ={
